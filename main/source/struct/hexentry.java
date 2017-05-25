7fefe334a3a26996f9512a911e0f3ced45e12bb4
stephenriblet <stephenriblet@gmail.com>
2017-05-25 18:19:24 -0400

    Create hexentry.java

---
+++ main/source/struct/hexentry.java
@@ -0,0 +1,14 @@
+public class hexentry implements {
+  private String value;
+  public hexentry(){
+    value= "";
+  }
+  public hexentry(String s){
+    value = s;
+  }
+  @overrides
+  public String toString(){
+    return value;
+  }
+
+}
